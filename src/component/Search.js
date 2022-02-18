f542ea60af48d36546ed5e1469e459fbdb69aaab
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2022-02-18 22:46:31 +0900

    Fix: Search.js 리팩토링

--- src/component/Search.js
+++ src/component/Search.js
@@ -10,7 +10,6 @@ function Search(){
     const [movies, setMovies] = useState([]);
     const onChange = (event)=>{
         setSearch(event.target.value);
-        console.log(event.target.value);
     }
     const onClick = ()=>{
         setBtn(true);
@@ -22,36 +21,30 @@ function Search(){
     const getMovie = async ()=>{
         setMovies([]);
         const json = await(await fetch(`https://www.omdbapi.com/?apikey=92e32667&s=${search}`)).json();
-        await (console.log("HI",json.Search))
         setMovies(json.Search);
     }
     useEffect(()=>{
-        console.log("um", btn, search);
         if(btn == true && search != ""){
-            console.log("y", btn, search);
             getMovie();
             setBtn(false);
         }
         else{
-            console.log("no", btn, search);
             setBtn(false);
         }
     }, [btn]);
-    console.log("무비: ",movies);
     return (
         <div>
             <form onSubmit={onSubmit} className={`${SearchStyles.search_bar}`}>
                 <input onChange={onChange} value={search} className={`${SearchStyles.search}`} type="text" placeholder="Search"/>
                 <button onClick={onClick} className={`${SearchStyles.btn}`}><SearchImg width="24" height="24" fill="#fff" /></button>
             </form>
-            {movies == [] || movies == undefined ?
+            {movies == undefined || movies.length == 0 ?
                 <div className={`${SearchStyles.nothing}`}>
                     <SearchImg width="250" height="250" />
                     <p>검색 결과가 없습니다. T . T</p>
                 </div>
                 :
                 <div className={SearchStyles.content}>
-                    {console.log("메롱", movies)}
                     {movies.map(v=>
                         <Movie  key={v.imdbID}
                             movie={movies}
