e54e847e765c4bc05e3428c3c7524acf2d532a65
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2022-02-20 19:38:09 +0900

    Fix: Favorite 로컬스토리지 값 불러오기, 값 삭제 및 저장

--- src/component/Favorite.js
+++ src/component/Favorite.js
@@ -2,44 +2,82 @@ import {useState, useEffect} from "react";
 import FavStyles from "./Favorite.module.css";
 import ScrollStyles from "./Scroll.module.css";
 import MovieListStyles from "./MovieList.module.css";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { ReactComponent as BookmarkColoredImg} from "../asset/Bookmark_colored.svg";
+
 
 function Favorite(){
+    const [favList, setFavlist] = useState(JSON.parse(localStorage.getItem("Favorite")));
+
+    const get_img = (Poster)=>{
+        if(Poster == "N/A"){
+            return {backgroundImage: `url(./no-image.png)`,}
+        }
+        else{
+            return {backgroundImage: `url(${Poster})`,}
+        }
+    }
+
+    const MySwal = withReactContent(Swal);
+    const delBookMark = (id, title, year, type, img)=>{
+        MySwal.fire({
+            width: 400,
+            padding: '1rem',
+            title: <p className={MovieListStyles.swalTitle}>즐겨찾기에서 삭제하시겠습니까?</p>,
+            showCancelButton: true,
+            confirmButtonText: "네 T . T",
+            confirmButtonAriaLabel: "즐겨찾기에서 삭제하기",
+            cancelButtonText: "취소",
+            cancelButtonAriaLabel: "즐겨찾기 삭제 취소하기",
+            confirmButtonColor: "#d33",
+        }).then((result)=>{
+            if (result.isConfirmed) {
+                const favObj = {
+                    Id: id,
+                    Title: title,
+                    Year: year,
+                    Type: type,
+                    Img: img,
+                }
+                const rmFavList = favList.filter((v)=>{
+                    return v.Id !== id;
+                });
+                console.log(rmFavList);
+                setFavlist(rmFavList);
+                localStorage.setItem("Favorite", JSON.stringify(favList));
+                if(rmFavList.length === 0){
+                    localStorage.removeItem("Favorite");
+                }
+            }
+        })
+    }
     return(
         <div>
-            <div className={`${FavStyles.title}`}>내 즐겨찾기</div>
+            <div className={FavStyles.title}>내 즐겨찾기</div>
             <div className={FavStyles.content}>
-                {/*{movies.map(v=> {*/}
-                {/*    return(*/}
-                {/*        <div id={v.imdbID} onClick={()=>doBookMark(v.imdbID, v.Title)} key={v.imdbID} className={SearchStyles.li}>*/}
-                {/*            <div className={MovieListStyles.mvImg} style={get_img(v.Poster)}></div>*/}
-                {/*            <div className={MovieListStyles.mvInfo}>*/}
-                {/*                <span className={MovieListStyles.mvFtStrong}>{v.Title}</span>*/}
-                {/*                <div className={MovieListStyles.mvFtSmallGroup}>*/}
-                {/*                    <span className={MovieListStyles.mvFtSmall}>{v.Year}</span>*/}
-                {/*                    <span className={MovieListStyles.mvFtSmall}>{v.Type}</span>*/}
-                {/*                </div>*/}
-                {/*            </div>*/}
-                {/*            {favList != null ?*/}
-                {/*                (favList.map((favListVal)=>{*/}
-                {/*                    return((favListVal.Id == v.imdbID)?*/}
-                {/*                        <div key={`${favListVal.Id}`}>*/}
-                {/*                            <BookmarkColoredImg className={MovieListStyles.bookMark} width='45' height='45' fill='#008aff' />*/}
-                {/*                        </div>*/}
-                {/*                        :*/}
-                {/*                        null)*/}
-                {/*                }))*/}
-                {/*                :*/}
-                {/*                null*/}
-                {/*            }*/}
-                {/*        </div>)*/}
-                {/*})}*/}
-                {/*/!*{movies == undefined || movies.length == 0 || totalPages < 11 ?*!/*/}
-                {/*/!*    null*!/*/}
-                {/*/!*    :*!/*/}
-                {/*/!*    <Paging page={page} totalResults={totalPages} handlePageChange={handlePageChange}/>*!/*/}
-                {/*/!*}*!/*/}
+                {favList == undefined || favList.length == 0 ?
+                    null
+                    :
+                    favList.map((val, index)=>{
+                        return(
+                            <div id={val.Id} onClick={()=>delBookMark(val.Id, val.Title, val.Year, val.Type, val.Img)} key={val.Id} className={MovieListStyles.li}>
+                                <div className={MovieListStyles.mvImg} style={get_img(val.Img)}></div>
+                                <div className={MovieListStyles.mvInfo}>
+                                    <span className={MovieListStyles.mvFtStrong}>{val.Title}</span>
+                                    <div className={MovieListStyles.mvFtSmallGroup}>
+                                        <span className={MovieListStyles.mvFtSmall}>{val.Year}</span>
+                                        <span className={MovieListStyles.mvFtSmall}>{val.Type}</span>
+                                    </div>
+                                </div>
+                                <BookmarkColoredImg className={MovieListStyles.bookMark} width='45' height='45' fill='#008aff' />
+                            </div>
+                        );
+                    })
+                }
             </div>
         </div>
+
     );
 }
 
