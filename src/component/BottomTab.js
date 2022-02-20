489a12171948af268597be7b85937e4f38f992cf
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2022-02-20 15:26:48 +0900

    Fix: BottomTab 탭 작동 구조 변경
    기존 구상 탭:
    1 Link나 A태그를 이용해 라우팅 설정
    2 검색란에 입력하면 클라이언트-사이드 라우팅을 주고 검색탭을 눌렀을때 홈 화면으로 가게 하려고 함
    3 근데 주소만 변경되고 콘텐츠는 안 변함
    새로운 탭:
    1 선택한 탭을 State로 저장한 뒤
    2 탭State가 변하면 보여주는 콘텐츠도 다르게 함

--- src/component/BottomTab.js
+++ src/component/BottomTab.js
@@ -1,14 +1,55 @@
 import {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
 import BtmStyles from "./BottomTab.module.css";
 import { ReactComponent as SearchImg} from "../asset/Search.svg";
 import { ReactComponent as BookmarkImg} from "../asset/Bookmark.svg";
+import Search from "./Search";
 
 function BottomTab(){
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const tabArr=[
+        {
+            tabTitle:(
+                <SearchImg width="40" height="40" fill="#ffffff80" />
+            ),
+            tabCont:(
+                <div> <Search /> </div>
+            )
+        },
+        {
+            tabTitle:(
+                <BookmarkImg width="38" height="38" fill="#ffffff80" />
+            ),
+            tabCont:(
+                <div> 즐겨찾기 </div>
+            )
+        }
+    ];
+
+    const tabClickHandler=(index)=>{
+        setActiveIndex(index)
+    }
+
     return (
-        <div className={`${BtmStyles.tab}`}>
-            <button className={`${BtmStyles.btn}`}><SearchImg width="40" height="40" fill="#fff" /></button>
-            <button className={`${BtmStyles.btn}`}><BookmarkImg width="38" height="38" fill="#fff" /></button>
+        <div>
+            <ul className={`${BtmStyles.tab}`}>
+                {tabArr.map((section, index)=>{
+                    return (
+                        <li key={index} onClick={()=>tabClickHandler(index)} className={activeIndex === index ? BtmStyles.btnActive : ""}>
+                            {section.tabTitle}
+                        </li>)
+                })}
+            </ul>
+            <div>
+                {tabArr[activeIndex].tabCont}
+            </div>
         </div>
+        // <div className={`${BtmStyles.tab}`}>
+        //     {/*<Link to={"/"} className={`${BtmStyles.btn}`}><SearchImg width="40" height="40" fill="#fff" /></Link>*/}
+        //     <button className={`${BtmStyles.btn}`}><SearchImg width="40" height="40" fill="#fff" /></button>
+        //     <button className={`${BtmStyles.btn}`}><BookmarkImg width="38" height="38" fill="#fff" /></button>
+        // </div>
     );
 }
 
